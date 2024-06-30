093569eae38d6c5925634c0537bc27bb125e98fa
marcos vinicius <marcusvinycius73@gmail.com>
2024-06-30 07:09:49 -0300

    initial commit

---
+++ ProvasEnem.Core/Models/ExamModel.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace ProvasEnem.Core.Models;
+
+public class ExamModel
+{
+    public int Id { get; set; }
+    public byte[]? ContentPdf { get; set; }
+    public string ColorNotebook { get; set; } = string.Empty;
+    public DateTime ExamDate { get; set; }
+
+}
