521fc560ebb4ba9f7d119982123c96b120381f2f
marcos vinicius <marcusvinycius73@gmail.com>
2024-06-30 07:42:26 -0300

    Feat: Create project API

--- ProvasEnem.Core/Models/ExamModel.cs
+++ ProvasEnem.Core/Models/ExamModel.cs
@@ -5,8 +5,7 @@ namespace ProvasEnem.Core.Models;
 public class ExamModel
 {
     public int Id { get; set; }
-    public byte[]? ContentPdf { get; set; }
+    public byte[] ContentPdf { get; set; } = null!;
     public string ColorNotebook { get; set; } = string.Empty;
     public DateTime ExamDate { get; set; }
-
 }
