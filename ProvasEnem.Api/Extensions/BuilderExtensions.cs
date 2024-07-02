e14a624093eb94e9a03ae49a308ff9dd9612d8be
marcos vinicius <marcusvinycius73@gmail.com>
2024-07-02 20:12:22 -0300

    feat: add Extensions

---
+++ ProvasEnem.Api/Extensions/BuilderExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProvasEnem.Api.Data;
+
+namespace ProvasEnem.Api.Extensions;
+
+public static class BuilderExtensions
+{
+    public static void AddSwagger(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen( x =>
+        {
+            x.CustomSchemaIds(n => n.FullName);
+        });
+    }
+
+    public static void AddDbContext(this WebApplicationBuilder builder)
+    {
+        var connectionString = builder
+                .Configuration
+                .GetConnectionString("Default") ?? string.Empty;
+
+        builder.Services.AddDbContext<AppDbContext>(x =>
+        {
+            x.UseSqlServer(connectionString);
+        });
+    }
+}
