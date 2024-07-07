c7c2bcda5176198835560559b17dca7ea70e0acf
marcos vinicius <marcusvinycius73@gmail.com>
2024-07-07 09:26:51 -0300

    Feat: Creat PagedResponse

--- ProvasEnem.Core/Requests/PagedRequest.cs
+++ ProvasEnem.Core/Requests/PagedRequest.cs
@@ -4,6 +4,6 @@ namespace ProvasEnem.Core.Requests;
 
 public abstract class PagedRequest : BasedRequest
 {
-    public int PagedNumber { get; set; } = 1;
-    public int PageSize { get; set; } = 25;
+    public int PagedNumber { get; set; } = Configuration.DefaultPageNumber;
+    public int PageSize { get; set; } = Configuration.DefaultPageSize;
 }
---
+++ ProvasEnem.Core/Responses/Response.cs
@@ -0,0 +1,29 @@
+﻿
+
+using System.Text.Json.Serialization;
+
+namespace ProvasEnem.Core.Responses;
+
+public class Response<TData>
+{
+    private readonly int _statusCode;
+
+    [JsonConstructor]
+    public Response()
+        => _statusCode = Configuration.DefaultStatusCode;
+    
+    public Response(TData? data, int statusCode = Configuration.DefaultStatusCode, string? message = null)
+    {
+        Data = data;
+        _statusCode = statusCode;
+        Message = message;
+    }
+
+    public TData? Data { get; set; }
+    public string? Message { get; set; }
+
+    [JsonIgnore]
+    public bool IsSucess 
+        => _statusCode is >= 200 and <= 299;
+
+}
